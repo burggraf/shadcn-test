465fa10b529b24c8a359ad428feaf2ea09a370d3
Mark Burggraf <markb@mantisbible.com>
2024-04-20 10:17:55 -0700

    renamed Menu1 to MenuFull, Menu2 to MenuSidebar

--- src/routes/customers/+page.svelte
+++ src/routes/customers/+page.svelte
@@ -1,34 +1,19 @@
 <script lang="ts">
-	import {
-		CircleUser,
-		LineChart,
-		Package,
-		Home,
-		ShoppingCart,
-		Bell,
-		Menu,
-		Package2,
-		Search,
-		Users,
-	} from 'lucide-svelte'
-	import { Badge, Button, Card, DropdownMenu, Input, Sheet } from '$lib'
-	import MenuContents from '$components/MenuContents.svelte'
-	import MenuFooter from '$components/MenuFooter.svelte'
+	import { Search } from 'lucide-svelte'
+	import { Button, Input } from '$lib'
 	import UserMenu from '$components/UserMenu.svelte'
-    import Menu1 from '$components/Menu1.svelte'
-	import Menu2 from '$components/Menu2.svelte'
+    import MenuFull from '$components/MenuFull.svelte'
+	import MenuSidebar from '$components/MenuSidebar.svelte'
 </script>
 
 <div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
 	<div class="hidden border-r bg-muted/40 md:block">
-		<!--MENU pane desktop-->
-        <Menu1 />
-		<!--end MENU pane desktop-->
+        <MenuFull />
 	</div>
 	<!--MAIN page-->
 	<div class="flex flex-col">
 		<header class="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-            <Menu2 />
+            <MenuSidebar />
             <div class="w-full flex-1">
 				<form>
 					<div class="relative">
