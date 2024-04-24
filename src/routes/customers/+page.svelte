858cf71605bb0e21c9b00d39df780bc44f6434c1
Mark Burggraf <markb@mantisbible.com>
2024-04-24 06:57:29 -0500

    text change

--- src/routes/customers/+page.svelte
+++ src/routes/customers/+page.svelte
@@ -15,12 +15,12 @@
 		<header class="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
             <MenuSidebar />
             <div class="w-full flex-1">
-				<form>
+				<form on:submit={()=>{console.log('submit')}}>
 					<div class="relative">
 						<Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
 						<Input
 							type="search"
-							placeholder="Search products..."
+							placeholder="Search customers..."
 							class="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
 						/>
 					</div>
