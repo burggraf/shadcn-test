b2de27dfe05e11af9998585784fcc746f882ac9e
Mark Burggraf <markb@mantisbible.com>
2024-04-30 06:59:31 -0500

    signin/signup toggle

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
     import { Dialog, Button, Card, Input, Label } from "$lib";
+    const setMode = (mode: string) => {
+        const signupCard = document.getElementById("signupCard");
+        const signinCard = document.getElementById("signinCard");
+        if (mode === "signup") {
+            signupCard?.classList.remove("hidden");
+            signinCard?.classList.add("hidden");
+        } else {
+            signupCard?.classList.add("hidden");
+            signinCard?.classList.remove("hidden");
+        }
+    };
 </script>
 <Dialog.Root>
     <Dialog.Trigger id="loginTrigger"></Dialog.Trigger>
     <Dialog.Content>
 
-      <Card.Root class="border-none">
+      <Card.Root class="border-none" id="signinCard">
         <Card.Header>
           <Card.Title class="text-2xl">Login</Card.Title>
           <Card.Description>Enter your email below to login to your account</Card.Description>
@@ -30,10 +41,46 @@
           </div>
           <div class="mt-4 text-center text-sm">
             Don&apos;t have an account?
-            <a href="/authentication02" class="underline"> Sign up </a>
+            <span on:click={()=>{setMode('signup')}} class="underline cursor-pointer"> Sign up </span>
           </div>
         </Card.Content>
       </Card.Root>
+
+      <Card.Root class="hidden border-none" id="signupCard">
+        <Card.Header>
+          <Card.Title class="text-2xl">Sign Up</Card.Title>
+          <Card.Description>Enter your information to create an account</Card.Description>
+        </Card.Header>
+        <Card.Content>
+          <div class="grid gap-4">
+            <div class="grid grid-cols-2 gap-4">
+              <div class="grid gap-2">
+                <Label for="first-name">First name</Label>
+                <Input id="first-name" placeholder="Max" required />
+              </div>
+              <div class="grid gap-2">
+                <Label for="last-name">Last name</Label>
+                <Input id="last-name" placeholder="Robinson" required />
+              </div>
+            </div>
+            <div class="grid gap-2">
+              <Label for="email">Email</Label>
+              <Input id="email" type="email" placeholder="m@example.com" required />
+            </div>
+            <div class="grid gap-2">
+              <Label for="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+            <Button type="submit" class="w-full">Create an account</Button>
+            <Button variant="outline" class="w-full">Sign up with Google</Button>
+          </div>
+          <div class="mt-4 text-center text-sm">
+            Already have an account?
+            <span on:click={()=>{setMode('signin')}} class="underline cursor-pointer"> Sign in </span>
+          </div>
+        </Card.Content>
+      </Card.Root>
+    
 
     </Dialog.Content>
   </Dialog.Root>
